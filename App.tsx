32fcfcc2d5332b271f18ee6f67d53c52f5c2fdbb
ukgrilss <contaprincipalpc9988@gmail.com>
2026-01-12 10:01:10 -0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BridgePage from './components/BridgePage';
+
+const App: React.FC = () => {
+  return (
+    <div className="antialiased">
+      <BridgePage />
+    </div>
+  );
+};
+
+export default App;
